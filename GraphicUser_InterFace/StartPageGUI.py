afbe64bb1885b2e5fde553b59d4e2abb02ce8343
Abhishek <abhishekrajput4736@gmail.com>
2023-03-23 15:04:58 +0530

    some changes in gui

--- GraphicUser_InterFace/StartPageGUI.py
+++ GraphicUser_InterFace/StartPageGUI.py
@@ -21,24 +21,24 @@ class App(customtkinter.CTk):
 
 
         self.sidebar_frame = customtkinter.CTkFrame(self, width=140, corner_radius=0)
-        self.sidebar_frame.grid(row=0, column=0, rowspan=4, sticky="nsew")
+        self.sidebar_frame.grid(row=0, column=0, rowspan=4, sticky="ew")
         self.sidebar_frame.grid_rowconfigure(4, weight=1)
         
         self.logo_label = customtkinter.CTkLabel(self.sidebar_frame, text="Profile", font=customtkinter.CTkFont(size=35, weight="bold"))
-        self.logo_label.grid(row=1, column=0, padx=20, pady=(20, 10))
+        self.logo_label.grid(row=1, column=0, padx=20, pady=20, sticky ="ew")
         
         self.appearance_mode_label = customtkinter.CTkLabel(self.sidebar_frame, text="Appearance Mode:", anchor="w")
-        self.appearance_mode_label.grid(row=5, column=0, padx=20, pady=(700, 0))
+        self.appearance_mode_label.grid(row=15, column=0, padx=20, pady=20)
         self.appearance_mode_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["Light", "Dark", "System"],
                                                                   command=self.change_appearance_mode_event)
-        self.appearance_mode_optionemenu.grid(row=6, column=0, padx=20, pady=(10, 10))
+        self.appearance_mode_optionemenu.grid(row=16, column=0, padx=20, pady =10, sticky ="ew")
         
         
         self.scaling_label = customtkinter.CTkLabel(self.sidebar_frame, text="UI Scaling:", anchor="w")
-        self.scaling_label.grid(row=7, column=0, padx=20, pady=(10, 0))
+        self.scaling_label.grid(row=17, column=0, padx=20, pady=10)
         self.scaling_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["80%", "90%", "100%", "110%", "120%"],
                                                                command=self.change_scaling_event)
-        self.scaling_optionemenu.grid(row=8, column=0, padx=20, pady=(10, 20))
+        self.scaling_optionemenu.grid(row=18, column=0, padx=20, pady=20, sticky ="ew")
 
 
         
@@ -48,7 +48,7 @@ class App(customtkinter.CTk):
         # self.upper_name_frame.grid_rowconfigure(6, weight=1)
         
         # self.project_name = customtkinter.CTkLabel(self.upper_name_frame, text="Unified Traveling and Transportation System (UTTS)", font=customtkinter.CTkFont(size=50, weight="bold"))
-        # self.project_name.grid(row=0, column=0, padx=200, pady=(50, 10))
+        # self.project_name.grid(row=0, column=5, padx=200, pady=50)
         
 
         # self.lower_frame = customtkinter.CTkFrame(self, width=140, corner_radius=5)
