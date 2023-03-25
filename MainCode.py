e9c68f37a8ce638fec64a3c1b1c63b371953b0f3
Harsh <harshshrivastava554@gmail.com>
2023-03-26 02:40:38 +0530

    created a table for user in database

--- MainCode.py
+++ MainCode.py
@@ -1,6 +1,11 @@
 import mysql.connector
-UTTSdb = mysql.connector.connect(host='localhost',user='root',password='harsh')
+UTTSdb = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='harsh',
+    database='UTTS')
 #print(UTTSdb.connection_id)
 #print to check connection establishment
 cur=UTTSdb.cursor()
-cur.execute("CREATE DATABASE UTTS")
+s="CREATE TABLE users(UserID INT,first_name varchar(25),last_name varchar(25),birth_date DATE,phone varchar(15),address varchar(50),city varchar(20),state varchar(25),points INT)"
+cur.execute(s)
