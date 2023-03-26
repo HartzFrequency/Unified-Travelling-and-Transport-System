119676ba0b61c98ff79f06bbb3213c539294a16a
Harsh <harshshrivastava554@gmail.com>
2023-03-26 22:09:33 +0530

    dummy values insertion in user table

--- MainCode.py
+++ MainCode.py
@@ -2,14 +2,11 @@ import mysql.connector
 UTTSdb = mysql.connector.connect(
     host='localhost',
     user='root',
-    password='*****',
+    password='harsh',
     database='UTTS')
 #print(UTTSdb.connection_id)
 #print to check connection establishment
 cur=UTTSdb.cursor()
-s="INSERT INTO users(first_name,last_name,birth_date,phone,address,city,state) VALUES(%s,%s,%s,%s,%s,%s,%s)"
 
-Value1=("harsh","shrivastava","2003-06-05",8109288418,"Hathi Khana Road Morar","Gwalior","MP")
 
-cur.execute(s,VALUES)
 UTTSdb.commit()
--- SQLquerries.py
+++ SQLquerries.py
@@ -6,7 +6,20 @@ DataInsertion="INSERT INTO users(first_name,last_name,birth_date,phone,address,c
 #cur.execute(s,VALUES)
 #to execute one tuple
 
-Values=[("harsh","shrivastava","2003-06-05",8109288418,"Hathi Khana Road Morar","Gwalior","MP")
-    ]
+s="INSERT INTO users(first_name,last_name,birth_date,phone,address,city,state) VALUES(%s,%s,%s,%s,%s,%s,%s)"
+
+Values=[("harsh","shrivastava","2003-06-05",8109288418,"Hathi Khana Road Morar","Gwalior","MP")]
 #cur.executemany(s,Values)
 #to insert many tuples we use this *list is passed 
+
+ManyValues=[("harsh","shrivastava","2003-06-05",8109288418,"Hathi Khana Road Morar","Gwalior","MP")
+    ("gauri","thakre","2003-11-30",9479675959,"Hostel no. 04, MITS","Gwalior","MP"),
+    ("akhil","jain","2003-08-05",7456025891,"Dal Bazar","Gwalior","MP"),
+    ("abhishek","rajput","2003-07-05",8109288418,"Kritika ke Dil me","Gwalior","MP"),
+    ("shahrukh","khan","1992-01-09",8827344852,"Gandhi Chowk Bazar","Chatarpur","MP"),
+    ("shraddha","kapoor","1985-08-30",6212418873,"Juhu","Mumbai","MH"),
+    ("sunny","deol","1983-12-25",6748319921,"Jalianvala Bagh","Amritsar","Punjab"),
+    ("hema","malini","1948-10-16",9828157533,"Kavi Bharti Nagar","Tiruchirapalli","Tamil Nadu"),
+    ("jethalal","gada","2004-12-02",7852773891,"Phool Bagh","Gwalior","MP"),
+    ("kartik","aryan","1995-02-14",7143143143,"Thatipur","Gwalior","MP")]
+#cur.executemany(s,Values)
