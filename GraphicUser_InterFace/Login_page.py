d4f04c991bdce06b0264122a3608e910c24c81f4
Harsh <harshshrivastava554@gmail.com>
2023-04-23 15:08:27 +0530

    enhancing login

--- GraphicUser_InterFace/Login_page.py
+++ GraphicUser_InterFace/Login_page.py
@@ -6,28 +6,31 @@ from PIL import Image, ImageTk
 import os
 from tkinter import PhotoImage
 from tkinter import messagebox
+from tkinter.font import Font
 
 customtkinter.set_appearance_mode("dark")
 
 class Login(customtkinter.CTk):
-    width = 1950
-    height = 1200
-
+    width = 1920
+    height = 1080
     def __init__(self):
         super().__init__()
 
-        self.title("Login Page")
-        self.geometry(f"{1700}x{580}")
+        # OPENEING WINDOW SIZE
+        self.title("Login")
+        self.geometry(f"{1920}x{1080}")
         self.bg_image = customtkinter.CTkImage(Image.open("Image/Background_gradient.jpg"),
                                                size=(self.width, self.height))
         self.bg_image_label = customtkinter.CTkLabel(self, image=self.bg_image)
         self.bg_image_label.grid(row=0, column=0)
 
-        # create login frame
-        self.login_frame = customtkinter.CTkFrame(self, corner_radius=0)
+        # LOGIN FRAME INSIDE WINDOW
+        # TEXT : "Welcome!\nUnified Travelling & Transport System"
+        self.login_frame = customtkinter.CTkFrame(self, corner_radius=20)
         self.login_frame.grid(row=0, column=0, sticky="ns")
-        self.login_label = customtkinter.CTkLabel(self.login_frame, text="Welcome To\nUnified-Travelling-and-Transport-System",
-                                                  font=customtkinter.CTkFont(size=50, weight="bold"))
+        self.login_label = customtkinter.CTkLabel(self.login_frame, text="Welcome!\nUnified Travelling & Transport System",font=customtkinter.CTkFont(size=24, weight="bold", slant="roman", family="Helvetica"))
+
+
         self.login_label.grid(row=0, column=0, padx=30, pady=(150, 15))
         self.login_label_2 = customtkinter.CTkLabel(self.login_frame, text="Login Page",font=customtkinter.CTkFont(size=40, weight="bold"))
         self.login_label_2.grid(row=1, column=0, padx=30, pady=(50, 15))
@@ -42,12 +45,11 @@ class Login(customtkinter.CTk):
     # def write_to_file(self):
         
     def login_event(self):
-
-        
+     
         UTTSdb = mysql.connector.connect(
         host='localhost',
         user='root',
-        password='Rajput@MySQL',
+        password='harsh',
         database='UTTS')
 
         entered_username = self.username_entry.get()
@@ -56,21 +58,17 @@ class Login(customtkinter.CTk):
         cur=UTTSdb.cursor()
         s="SELECT * FROM users WHERE first_name = '{}' AND Password = '{}'".format(entered_username,entered_password)
         cur.execute(s)
-        # QueryCheckForPassword=cur.fetchall()
         QueryCheckForPassword=cur.fetchone()
-        # print(QueryCheckForPassword)
 
         if QueryCheckForPassword:
             self.destroy()            
             import StartPageGUI
             StartPageGUI.Main().mainloop()
+
         else:
             print("error")
             return messagebox.showerror('Error','Incorrect Username or Password')
         
-
-
-                
         # file = open("LocalDATA//password.txt", "w")
         # file.write(entered_password)
         # file.close()
@@ -78,7 +76,6 @@ class Login(customtkinter.CTk):
         # file = open("LocalDATA//username.txt", "w")
         # file.write(entered_username)
         # file.close()
-
         print("Login pressed - username:", entered_username, "password:",entered_password)
 
 if __name__ == "__main__":
