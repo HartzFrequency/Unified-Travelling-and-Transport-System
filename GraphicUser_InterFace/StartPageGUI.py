9f80f4562cb7c3c40a41fc9077acac93927f3091
Harsh <harshshrivastava554@gmail.com>
2023-04-24 20:00:22 +0530

    UI

--- GraphicUser_InterFace/StartPageGUI.py
+++ GraphicUser_InterFace/StartPageGUI.py
@@ -22,51 +22,39 @@ class Main(customtkinter.CTk):
         self.geometry(f"{1240}x{720}")
 
         # configure grid layout (4x4)
-        self.grid_columnconfigure(1, weight=1)
-        self.grid_columnconfigure((2, 3), weight=0)
+        self.grid_columnconfigure(1, weight=0)
+        self.grid_columnconfigure((2, 3), weight=1)
         self.grid_rowconfigure((0, 1, 2), weight=1)
 
-        #Sidebar frame
-        # self.sidebar_frame = customtkinter.CTkFrame(self, width=140, corner_radius=0)
-        # self.sidebar_frame.grid(row=0, column=0, rowspan=4, sticky="nsew")
-        # self.sidebar_frame.grid_rowconfigure(4, weight=1)
-        
 
-        self.top_left_corner_frame = customtkinter.CTkFrame(self)
-        self.top_left_corner_frame.grid(row=0,column=0)
-        # PROFILE BUTTON
-        self.profile_button = customtkinter.CTkButton(self.top_left_corner_frame,text="Profile")
-        self.profile_button.grid(row=0,column=0,padx=10,pady=15)
-        # LOGIN BUTTON
-        self.back_to_loginPage_button = customtkinter.CTkButton(self.top_left_corner_frame,text="Log Out", command=self.open_Login_window)
-        self.back_to_loginPage_button.grid(row=1, column=0, padx=10, pady=10)
-
-
-        # self.logo_label = customtkinter.CTkLabel(self.sidebar_frame, text="Profile", font=customtkinter.CTkFont(size=35, weight="bold"))
-        # self.logo_label.grid(row=0, column=0, padx=20, pady=20, sticky ="ew")
-
-
-
-        self.sidebar_frame = customtkinter.CTkFrame(self, width=120, corner_radius=0)
+        self.sidebar_frame = customtkinter.CTkFrame(self, width=120, corner_radius=15)
         self.sidebar_frame.grid(row=9, column=0, rowspan=4, sticky="nsew")
-        self.sidebar_frame.grid_rowconfigure(4, weight=1)
+        self.sidebar_frame.grid_rowconfigure(1, weight=1)
         
-        
+        # PROFILE BUTTON
+        self.profile_button = customtkinter.CTkButton(self.sidebar_frame,text="Profile")
+        self.profile_button.grid(row=0,column=0,padx=10,pady=15)
+
+        # LOGIN BUTTON
+        self.back_to_loginPage_button = customtkinter.CTkButton(self.sidebar_frame,text="Log Out", command=self.open_Login_window)
+        self.back_to_loginPage_button.grid(row=1, column=0, padx=10, pady=15)
+
+        # APPEARANCE TEXT
         self.appearance_mode_label = customtkinter.CTkLabel(self.sidebar_frame, text="Appearance Mode:", anchor="w")
-        self.appearance_mode_label.grid(row=5, column=0, padx=20, pady=(10,0))
+        self.appearance_mode_label.grid(row=5, column=0, padx=10, pady=(5,0))
+        # APPEARANCE BUTTON
         self.appearance_mode_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["Light", "Dark", "System"],command=self.change_appearance_mode_event)
-        self.appearance_mode_optionemenu.grid(row=6, column=0, padx=20, pady =(10,10), sticky ="ew")
+        self.appearance_mode_optionemenu.grid(row=6, column=0, padx=20, pady =(10,10))
         
-        
-        self.scaling_label = customtkinter.CTkLabel(self.sidebar_frame, text="UI Scaling:", anchor="w")
+        # SCALING TEXT
+        self.scaling_label = customtkinter.CTkLabel(self.sidebar_frame, text="UI Scaling:")
         self.scaling_label.grid(row=7, column=0, padx=20, pady=(10,0))
-        self.scaling_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["70%", "80%", "90%", "100%", "110%", "120%", "130%"],
-                                                               command=self.change_scaling_event)
-        self.scaling_optionemenu.grid(row=8, column=0, padx=20, pady=(10,20), sticky ="ew")
+        # SCALING BUTTON
+        self.scaling_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["70%", "80%", "90%", "100%", "110%", "120%", "130%"],command=self.change_scaling_event)
+        self.scaling_optionemenu.grid(row=8, column=0, padx=20, pady=(5,20))
 
         self.appearance_mode_optionemenu.set("Dark")
-        self.scaling_optionemenu.set("110%")
-
+        self.scaling_optionemenu.set("100%")
 
         
         
