d2f881de2edacb2837ba0dddf58fd6afbf83c6f9
Harsh <harshshrivastava554@gmail.com>
2023-04-30 01:24:27 +0530

    modification in display

--- GraphicUser_InterFace/Display_availability.py
+++ GraphicUser_InterFace/Display_availability.py
@@ -5,25 +5,25 @@ class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
 
-        self.geometry("500x300")
-        self.title("small example app")
+        self.geometry("700x300")
+        self.title("Route Information Page")
         self.minsize(300, 200)
 
-        # create 2x2 grid system
         self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure((0, 1), weight=1)
 
         self.textbox = customtkinter.CTkTextbox(master=self)
         self.textbox.grid(row=0, column=0, columnspan=2, padx=20, pady=(20, 0), sticky="nsew")
 
-        #writing availabilty in text box
         AvailableList = sql.Query_FetchFromFile()
+        indexing_Choices=1
         for i in range(AvailableList.__len__()):
-            self.textbox.insert("0.0",AvailableList[i]+"\n")
-
-        self.combobox = customtkinter.CTkComboBox(master=self, values=["Sample text 1", "Text 2"])
+            self.textbox.insert("0.0",str(indexing_Choices)+"). "+AvailableList[i]+"\n")
+            indexing_Choices=indexing_Choices+1        
+        list=["Sample text 1", "Text 2", "harsh"]
+        self.combobox = customtkinter.CTkComboBox(master=self, values=list)
         self.combobox.grid(row=1, column=0, padx=20, pady=20, sticky="ew")
-        self.button = customtkinter.CTkButton(master=self, command=self.button_callback, text="Insert Text")
+        self.button = customtkinter.CTkButton(master=self, command=self.button_callback, text="Proceed")
         self.button.grid(row=1, column=1, padx=20, pady=20, sticky="ew")
 
     def button_callback(self):
