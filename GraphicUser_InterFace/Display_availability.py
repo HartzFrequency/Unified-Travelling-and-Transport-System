1c4f69e53ee169f6da6d234b8c2521460c296fb4
Shreya H S <135348911+shrehs@users.noreply.github.com>
2024-05-16 20:54:34 +0530

    Update Display_availability.py

--- GraphicUser_InterFace/Display_availability.py
+++ GraphicUser_InterFace/Display_availability.py
@@ -3,6 +3,14 @@ import Modules.SQL as sql
 import Modules.FileHandling as File
 
 class Available(customtkinter.CTk):
+    """
+    Class representing the Route Information Page GUI.
+
+    Attributes:
+        textbox: Textbox to display available routes.
+        combobox: ComboBox to select a specific route.
+        button: Button to proceed with the selected route.
+    """
     def __init__(self):
         super().__init__()
 
@@ -13,11 +21,12 @@ class Available(customtkinter.CTk):
         self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure((0, 1), weight=1)
 
+        # Textbox for displaying available routes
         self.textbox = customtkinter.CTkTextbox(master=self)
         self.textbox.grid(row=0, column=0, columnspan=2, padx=20, pady=(20, 0), sticky="nsew")
         self.textbox.configure(font=("Arial", 18))
 
-
+        # Fetching available routes from the file and displaying them
         AvailableList = sql.Query_FetchFromFile()
         AvailableList.reverse()
         list=[]
@@ -29,13 +38,18 @@ class Available(customtkinter.CTk):
         list.append("Select")
         list.reverse()
 
+        # ComboBox for route selection
         self.combobox = customtkinter.CTkComboBox(master=self, values=list)
         self.combobox.grid(row=1, column=0, padx=20, pady=20, sticky="ew")
+        # Button to proceed with the selected route
         self.button = customtkinter.CTkButton(master=self, command=self.button_callback, text="Proceed")
         self.button.grid(row=1, column=1, padx=20, pady=20, sticky="ew")
         
 
     def button_callback(self):
+        """
+        Callback function for the button to handle the selected route and proceed to the payment page.
+        """
         selection = self.combobox.get()
         File.Handle_Selection(selection)
         self.destroy()            
